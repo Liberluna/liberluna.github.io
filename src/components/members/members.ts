ef92645907f418befd10f52564d45313e4a7dca4
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-05-17 18:06:24 +0900

    Update members.ts

--- src/components/members/members.ts
+++ src/components/members/members.ts
@@ -15,7 +15,8 @@ export type Profile = {
     url: string,
     icon: string,  // 白黒画像
     name: string,
-  }>
+  }>,
+  TBD?: string // 更新予定
 }
 export type Members = Array<Profile>
 
@@ -69,6 +70,7 @@ const members = (): Members => [
         icon: "https://cdn.jsdelivr.net/npm/simple-icons@8.8.0/icons/twitter.svg",
       },
     ],
+    TBD: "はやくアイコン決めてくれ・・・"
   },
   {
     name: "Bonds",
