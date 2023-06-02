dd5063726bef40e4e16ed0e21c555cfd2a0a28e5
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-02 22:04:49 +0900

    Update server.ts

--- deno/server.ts
+++ deno/server.ts
@@ -8,13 +8,7 @@ import { serveStatic } from 'https://deno.land/x/hono@v3.2.0-rc.3/middleware.ts'
 const app = new Hono()
 
 app.get(
-  '/github',
-  (context) => {
-    context.response.redirect('https://github.com/Liberluna/liberluna.github.io/')
-  }
-)
-
-app.use(
+  '/*',
   serveStatic({
     root: './dist',
   })
