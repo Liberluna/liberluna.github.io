c62bf7a6d694cba275ab3683612656c276c620ad
nakasyou <syoutarou1103@outlook.jp>
2023-06-03 11:05:43 +0000

    Add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import sitemap from "@astrojs/sitemap";
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap"
+import tailwind from '@astrojs/tailwind'
+import vue from '@astrojs/vue'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+	  tailwind(),
+	  sitemap(),
+	  vue(),
+  ],
   base: '/',
   site: "https://liberluna.github.io"
 });
