2a9e6d9bb18476cdeb2503e99801f587f3ccaff8
ぽち@Liberluna <121510825+Pochi-Liberluna@users.noreply.github.com>
2023-06-19 08:53:25 +0900

    Update projects.ts

--- datas/projects.ts
+++ datas/projects.ts
@@ -6,6 +6,11 @@ export interface Project {
 
 export const projects: Project[] = [
   {
+    title: "LiberChat",
+    desc: "現在Liberlunaが開発中のオンラインチャットサービス、「LiberChat」。開発段階です。",
+    link: "https://liberchat.deno.dev/",
+  },
+  {
     title: "LunaNovaCSS",
     desc: "JSでObjectをCSSに動的に変換する 新感覚のCSSフレームワーク",
     link: "https://github.com/Liberluna/LunaNovaCSS",
